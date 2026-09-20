9a58a5cfe9904d154cf42e63e04ff9fffe14af8a
shripawar334 <shripadpawar.skn.comp@gmail.com>
2026-09-20 09:52:38 +0530

    added url in main

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://fraud-guard-ai-silk.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
