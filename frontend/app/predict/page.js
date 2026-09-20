42628be89f9da82af73e6dd95bf2a78855916622
shripawar334 <shripadpawar.skn.comp@gmail.com>
2026-09-20 10:02:59 +0530

    frontend predict-changes

--- frontend/app/predict/page.js
+++ frontend/app/predict/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const API_URL = "http://127.0.0.1:8000/predict";
+const API_URL = "https://fraudguard-ai-production.up.railway.app/predict";
 
 export default function Predict() {
   const [formData, setFormData] = useState({
